b18c307a1f3e90e16ea8f43ab297f8d901f9461d
AnujDeshwal <anujdeshwal95@gmail.com>
2024-09-08 11:12:19 +0530

    Server added

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Toaster } from "react-hot-toast";
 import {BrowserRouter as Router , Routes , Route} from "react-router-dom";
 import Home from './pages/Home';
 import Main from './pages/main';
+import Output from './pages/Output';
 const App = () => {
   return (
     <Router>
     <Routes>
       <Route path="/" element={<Home/>}></Route>
       <Route path="/main" element={<Main/>}></Route>
+      <Route path="/output" element={<Output/>}></Route>
     </Routes>
     <Toaster position="bottom-center" />
    </Router>
